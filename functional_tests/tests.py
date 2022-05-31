bb718a4caa94cd574dcced11b56650d784557bed
zny <1357000948@qq.com>
2022-05-31 13:05:00 +0800

    new URL,view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -30,6 +30,34 @@ class NewVisitorTest(LiveServerTestCase):# (1)
 
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get(self.live_server_url)
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)  # (4)
+        header_text = self.browser.find_element_by_tag_name('h1').text  # 1
+        self.assertIn('To-Do', header_text)
+
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')  # 1
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures）
+        inputbox.send_keys('Buy peacock feathers')  # 2
+
+        # when she hits enter，the page updates，and now the page lists
+        # "1:Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)  # 3
+
+        self.wait_for_row_in_list_table('1:Buy peacock feathers')
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        
         # The page updates again, and now shows both items on her list
         self.wait_for_row_in_list_table('1:Buy peacock feathers')
         self.wait_for_row_in_list_table('2:Use peacock feathers to make a fly')
