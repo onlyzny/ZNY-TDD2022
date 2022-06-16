39e5e0cd0e9dc623f4d82b2884f431a2d250b245
onlyzny <107540858+onlyzny@users.noreply.github.com>
2022-06-16 21:31:43 +0800

    Update settings.py

--- suplerlists/settings.py
+++ suplerlists/settings.py
@@ -25,7 +25,7 @@ CSRF_TRUSTED_ORIGINS=['http://8.142.68.234']
 SECRET_KEY = 'django-insecure-9kq1p@jx20y-p)+q8om)ug!)k#bjv@f0__m6g0o6+(sq@zs_ul'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8.142.68.234']
 
